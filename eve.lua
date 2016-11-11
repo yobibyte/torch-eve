1f3e81987196b44fed8153d85146fa84a20f955f
yobibyte <vitaliykurin@gmail.com>
2016-11-11 01:05:58 +0100

    Fix the optimizer. Add mnist code.

--- eve.lua
+++ eve.lua
@@ -1,3 +1,4 @@
+require 'optim'
 -- adam code taken from https://raw.githubusercontent.com/torch/optim/master/adam.lua
 
 --[[n implementation of Adam http://arxiv.org/pdf/1412.6980.pdf
@@ -26,7 +27,7 @@ RETURN:
 
 ]]
 
-function optim.adam(opfunc, x, config, state)
+function optim.eve(opfunc, x, config, state)
    -- (0) get/update state
    local config = config or {}
    local state = state or config
@@ -41,7 +42,6 @@ function optim.adam(opfunc, x, config, state)
 
    local k_lower = config.k_lower or 0.1
    local k_upper = config.k_upper or 10
-
 
    -- (1) evaluate f(x) and df/dx
    local fx, dfdx = opfunc(x)
@@ -61,46 +61,42 @@ function optim.adam(opfunc, x, config, state)
    state.denom = state.denom or x.new(dfdx:size()):zero()
    -- relative change moving average
    state.d = state.d or 1
-   -- objective
-   state.f_prev = state.f_prev or x.new(dfdx:size()):zero()
-   -- objective
-   state.f_preprevv = state.f_preprev or x.new(dfdx:size()):zero()
+   -- objective value at t-1
+   state.f_prev = state.f_prev or 0
+   -- objective value at t-2
+   state.f_preprevv = state.f_preprev or 0
 
-
-   -- (3) learning rate decay (annealing)
-   local clr = lr / (1 + state.t*lrd)
 
    state.t = state.t + 1
 
    -- Decay the first and second moment running average coefficient
    state.m:mul(beta1):add(1-beta1, dfdx)
    state.v:mul(beta2):addcmul(1-beta2, dfdx, dfdx)
+   local m_corrected = torch.div(state.m, 1-beta1^state.t)
+   local v_corrected = torch.div(state.v, 1-beta2^state.t) 
 
    if state.t > 1 then
-
       local delta_lower = k_lower+1
-      local delta_upper = k+upper+1
-      if fx <= state.f then
-         delta_lower = 1/(k_lower+1)
-         delta_upper = 1/(k_upper+1)         
-      local c = min(max(delta_lower, fx/state.f_preprev), delta_upper)  
-      local tmp = state.f_prev
-      state.f_prev = c*state.f_preprev
-      state.f_preprev = tmp
-      local ri
-      r:copy(state.f_prev):csub(state.f_preprev):abs()/torch.cmin(state.f_prev, state.f_preprev)
-      state.d:mul(beta3):add(1-beta3):mul(r)
-      local biasCorrection1 = 1 - beta1^state.t
-      local biasCorrection2 = 1 - beta2^state.t
-      local stepSize = clr * math.sqrt(biasCorrection2)/biasCorrection1
-   else:
+      local delta_upper = k_upper+1
+      if fx <= state.f_preprev then
+         delta_lower = 1/(k_upper+1)
+         delta_upper = 1/(k_lower+1)         
+      end
+      local c = torch.min(torch.Tensor{torch.max(torch.Tensor{fx/state.f_preprev, delta_lower}), delta_upper})  
+      state.f_preprev = state.f_prev
+      state.f_prev = c*state.f_prev
+      local r = torch.abs(state.f_prev-state.f_preprev)/torch.min(torch.Tensor{state.f_prev, state.f_preprev})
+      state.d = beta3*state.d + (1-beta3)*r
+   else
       state.f_preprev = state.f_prev
       state.f_prev    = fx 
       state.d = 1
+   end
 
-   state.denom:copy(state.d:mul(state.v:sqrt()):add(epsilon))
+   state.denom:copy(v_corrected):sqrt():mul(state.d):add(epsilon)
+
    -- (4) update x
-   x:addcdiv(-stepSize, state.m, state.denom)
+   x:addcdiv(-lr, m_corrected, state.denom)
 
    -- return x*, f(x) before optimization
    return x, {fx}
